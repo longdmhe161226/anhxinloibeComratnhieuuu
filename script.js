6948bebea6848ee949be0db8b75f3ca524f62fb0
Nong2km <100985816+longdmhe161226@users.noreply.github.com>
2025-03-05 22:15:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "AnhyeubeComnhieulammmm ,Anh hứa không làm bé buồn nữa đâu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
-    "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "Anh biết lỗi rồi :<<",
+    "Mong bé tha lỗi cho anhh :((",
+    "Anhh sai rồi , anhh đáng trách huhu",
+    "Bé đừng giận anhh nữa nhaaa",
+    "AnhyeubeComnhieulammmmm",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
